5f5d3833feec2b4cecc44b5fdd326d262968e6d0
parkgeon7573 <parkrjsgur7573@naver.com>
2022-07-24 14:15:04 +0900

    unity0724

--- My project/Assets/Script/PlayerController.cs
+++ My project/Assets/Script/PlayerController.cs
@@ -4,20 +4,55 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    Animator m_animator;
+    [SerializeField]
+    SpriteRenderer m_spriteRenderer;
+    [SerializeField]
+    float m_speed = 0.02f;
+    [SerializeField]
+    Rigidbody2D m_regidbody;
+    Vector3 m_dir;
+
+    public float Speed { get { return m_speed; } set { m_speed = value; } }
+
     void MovePlayer()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
         {
-            gameObject.transform.position += new Vector3(-1f, 0f, 0f) * 0.02f;
+            m_animator.SetBool("IsMove", false);
+            m_dir = Vector3.zero;
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            gameObject.transform.position += new Vector3(1f, 0f, 0f) * 0.02f;
+            m_animator.SetBool("IsMove", true);
+            m_spriteRenderer.flipX = false;
+            m_dir = Vector3.left;
         }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            m_animator.SetBool("IsMove", true);
+            m_spriteRenderer.flipX = true;
+            m_dir = Vector3.right;
+        }
+        gameObject.transform.position += m_dir * Speed * Time.deltaTime;
     }
+    #region Unity Methods
     // Start is called before the first frame update
     void Start()
     {
+        m_animator = gameObject.GetComponent<Animator>();
+        m_spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        m_regidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            m_regidbody.AddForce(Vector2.up * 15f, ForceMode2D.Impulse);
+            //m_regidbody.velocity += (Vector2)m_dir * m_speed * Time.fixedDeltaTime;
+        }
 
     }
 
@@ -26,4 +61,5 @@ public class PlayerController : MonoBehaviour
     {
         MovePlayer();
     }
+    #endregion
 }
