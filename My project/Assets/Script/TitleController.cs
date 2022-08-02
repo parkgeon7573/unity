a90547342b7642316b8b98e576c1b9e8df682e81
parkgeon7573 <prkrjsgur7573@naver.com>
2022-08-02 17:53:21 +0900

    0802

--- My project/Assets/Script/TitleController.cs
+++ My project/Assets/Script/TitleController.cs
@@ -4,12 +4,53 @@ using UnityEngine;
 
 public class TitleController : MonoBehaviour
 {
+    
+    bool[] m_isActive = { false, false };
+    bool m_isopen;
+    string m_id = "아이디를 입력하세요";
+    string m_pass = string.Empty;
+    
+    string[] m_weaponList = { "단검", "한손검", "양손검", "양손도끼", "할버드", "레이저건" };
+    float m_height = 30;
+    int m_weaponIndex;
     void OnGUI()
     {
         if(GUI.Button(new Rect((Screen.width - 200) / 2, (Screen.height - 50) / 2, 200, 50), "START"))
         {
             Debug.Log("버튼 눌림");
         }
+        GUILayout.BeginArea(new Rect(10, Screen.height - 300, 200, 300),GUI.skin.box);
+        GUILayout.Space(10);
+        GUILayout.Button("START");
+        m_isActive[0] = GUILayout.Toggle(m_isActive[0], "무적모드");
+        if(m_isActive[0])
+        {
+            GUILayout.TextArea("무적모드 활성화");
+        }        
+        m_isActive[1] = GUILayout.Toggle(m_isActive[1], "파워모드");
+        if (m_isActive[1])
+        {
+            GUILayout.TextArea("파워모드 활성화");
+        }
+        GUILayout.Label("Id");
+        m_id = GUILayout.TextField(m_id, 20);
+        GUILayout.Label("Pass");
+        m_pass = GUILayout.PasswordField(m_pass, '*', 8);        
+        GUILayout.EndArea();
+        GUILayout.BeginArea(new Rect(Screen.width - 10 - 200, Screen.height - m_height, 200, 300), GUI.skin.box);
+        m_isopen = GUILayout.Toggle(m_isopen, "무기목록", GUI.skin.button);
+        if(m_isopen)
+        {
+            m_height = 300;
+            m_weaponIndex = GUILayout.SelectionGrid(m_weaponIndex, m_weaponList, 1);
+        }
+        else
+        {
+            m_height = 30;
+        }
+        GUILayout.EndArea();
+    
+
     }
     // Start is called before the first frame update
     void Start()
@@ -22,4 +63,5 @@ public class TitleController : MonoBehaviour
     {
         
     }
+    
 }
